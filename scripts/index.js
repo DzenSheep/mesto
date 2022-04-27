73d24892e1b49db46016d26783d4157c28ea13e0
Sergey Dolgikh <DzenSheep@yandex.ru>
2022-04-27 21:53:34 +0300

    На третью проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,20 @@
+let formSubmit = document.querySelector('.popup__form'); // Находим всю форму
 let nameInput = document.getElementById('name'); // Находим поле name формы в DOM
 let jobInput = document.getElementById('about'); // Находим поле about формы в DOM
 let profileName = document.querySelector('.profile__name'); // Выбираем то, что сейчас написано в Имени профиля
 let profileAbout = document.querySelector('.profile__about'); // Выбираем то, что сейчас написано в О профиле
 let popup = document.querySelector('.popup'); //Выбираем весь контейнер попапа
-
-let popupSave = document.querySelector('.popup__save'); // Выбираем кнопку сохранить
 let profileEdit = document.querySelector('.profile__edit'); // Выбираем кнопку Редактировать профиль
 let popupClose = document.querySelector('.popup__close'); //Выбираем кнопку закрыть
 
-function popupActive () {
+function popupOpen () {
   popup.classList.toggle('popup_value-active');
   nameInput.value = profileName.textContent
   jobInput.value = profileAbout.textContent
+}
+
+function popupCancel () {
+  popup.classList.toggle('popup_value-active');
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -22,12 +25,12 @@ function formSubmitHandler (evt) {
                                                 // О том, как это делать, расскажем позже.
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    popup.classList.toggle('popup_value-active');
+    popupCancel ()
 }
 
-profileEdit.addEventListener('click', popupActive);
-popupClose.addEventListener('click', popupActive);
-popupSave.addEventListener('click', formSubmitHandler);
+profileEdit.addEventListener('click', popupOpen);
+popupClose.addEventListener('click', popupCancel);
+formSubmit.addEventListener('submit', formSubmitHandler);
 
 
 // const like = document.querySelector('.Elements__like'); // Закоментировали лайк 
